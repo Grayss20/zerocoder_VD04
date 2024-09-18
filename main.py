4a65bae1a382c23fa75e5fd1ba11e41788875ebe
Grayss20 <157813957+Grayss20@users.noreply.github.com>
2024-09-18 22:30:42 +0100

    fin

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def three_persons():
     return render_template('index.html')
 
+@app.route('/ocuppations/')
+def ocuppations():
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run()
