47c433e106430f64467a748a0f2757a27158ee59
Grayss20 <157813957+Grayss20@users.noreply.github.com>
2024-09-21 21:49:53 +0100

    vd05 amendments

--- main.py
+++ main.py
@@ -2,13 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def three_persons():
-    return render_template('index.html')
+    context = {
+        'caption': '3 persons',
+        'city': 'Новосибирск'
+    }
+    return render_template('index.html', **context)
+
+
+@app.route('/shablon/')
+def three_persons_2():
+    context = {
+        'caption': '3 человека',
+        'city': 'FGDAFYERYyj'
+    }
+    return render_template('index.html', **context)
+
 
 @app.route('/ocuppations/')
 def ocuppations():
     return render_template('main.html')
+
 
 if __name__ == '__main__':
     app.run()
